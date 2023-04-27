314ec3dff4d32e29338190236c3175902a2cf107
JeongSang <gg78700@gmail.com>
2023-04-27 10:01:19 +0900

    Add files via upload

---
+++ Simple_Profile/Biggo.jsx
@@ -0,0 +1,12 @@
+// import React from "react"
+
+
+// export default function Biggo({image,isNew}){
+//     return (
+//         <div class = "Biggo">
+//             <img className = "photo" src = {image} alt = "아이유" />
+//             {isNew && <span className = "new">New</span>}
+//         </div>
+//     )
+// }
+
---
+++ Simple_Profile/Profile.jsx
@@ -0,0 +1,40 @@
+// export default function Profile(props){
+//     return(
+//         <div className = "profile">
+//             <img className = "photo" src = {props.image}/>
+//             <h1>{props.name}</h1>
+//             <p>{props.title}</p>
+
+
+            
+//         </div>
+//     )
+// }
+// props 이름만 작성하고 값 설정을 생략한다면, 이를 true 로 설정한 것으로 간주합니다.
+
+// import React from "react"
+// import Biggo from "./Biggo"
+
+
+
+// export default function Profile({image,name,title,isNew}){
+//     return(
+//         <div className = "profile">
+//             <Biggo image = {image} isNew = {isNew}/>
+//             <h1>{name}</h1>
+//             <p>{title}</p>
+            
+//         </div>
+//     )
+// }
+
+
+/* <div className = "profile">
+<img className = "photo" src = {image} alt = "아이유" />
+            {isNew && <span className = "new">New</span>}
+<h1>{name}</h1>
+<p>{title}</p>
+
+
+</div> */
+
